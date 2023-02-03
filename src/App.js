5053f124dd7040a2379e24d3422a7eaf550a1f2f
MEdaHad <riot@D4R>
2023-02-03 23:41:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import {
+  urlClient,
+  LENS_HUB_CONTRACT_ADDRESS,
+  queryRecommendedProfiles,
+  queryExplorerPublications
+} from './queries';
+
+import LENSHUB from "./lenshub.json";
+import { ethers } from "ethers";
+import { Box, Button, Image } from "@chakra-ui/react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [account, setAccount] = useState(null);
+  const [profiles, setProfiles] = useState([]);
+  const [posts, setPosts] = useState([]);
+  async function signIn() {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts"
+    });
+    setAccount(accounts[0]);
+  }
+  async function getRecommendedProfiles(){
+    const response = await urlClient.query(queryRecommendedProfiles).toPromise();
+  }
 
+  return <div className="app"></div>;
+}
 export default App;
